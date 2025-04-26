9fa86c36f703b0c2a24f06d754c7d9ac32b08890
Abdulla Alheela <abdulla.alheela@gmail.com>
2025-04-26 20:56:19 +0300

    initial setup

---
+++ Booked_Up/Booked_Up/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Booked_Up project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Booked_Up.settings')
+
+application = get_asgi_application()
