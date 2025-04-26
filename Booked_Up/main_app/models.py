47ae8bcd51f44988cda6c0d700077cc6c86b915f
Abdulla Alheela <abdulla.alheela@gmail.com>
2025-04-26 23:20:43 +0300

    added detail page

--- Booked_Up/main_app/models.py
+++ Booked_Up/main_app/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     is_available = models.BooleanField()
     created_at = models.DateField(auto_now_add=True)
-    image = models.CharField()
+    image = models.TextField()
 
     def __str__(self):
         return f"{self.title} written by  {self.author}"
+    
+    def get_absolute_url(self):
+
+        return reverse('book-detail', kwargs={'book_id': self.id})
--- Booked_Up/main_app/views.py
+++ Booked_Up/main_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Book
 
 def home(request):
     return render(request, 'home.html')
@@ -9,4 +8,9 @@ def about(request):
     return render(request, 'about.html')
 
 def book_index(request):
+    books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', {'book': book})
