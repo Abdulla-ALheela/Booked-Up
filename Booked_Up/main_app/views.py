6d389b2e2edd97295e4609255b243462732827bb
Abdulla Alheela <abdulla.alheela@gmail.com>
2025-04-26 22:04:03 +0300

    CSS and index.html added

--- Booked_Up/main_app/views.py
+++ Booked_Up/main_app/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 
 
 def home(request):
-   
     return render(request, 'home.html')
-
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    return render(request, 'books/index.html', {'books': books})
