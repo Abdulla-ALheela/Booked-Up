5268d150c6fa8e9574cd0a8c344e9528d8a14fc1
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-04-28 23:09:03 +0300

    added index.html for BorrowCart model and completed add and delete items from cart with CSS

--- Booked_Up/main_app/views.py
+++ Booked_Up/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Book
+from .models import Book,BorrowCart,BorrowList
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404
 
 
 def home(request):
@@ -24,6 +25,37 @@ def book_index(request):
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'books/detail.html', {'book': book})
+
+@login_required
+def cart_index(request):
+    cart = request.session.get('cart', [])
+    books = Book.objects.filter(id__in=cart)
+    return render(request, 'cart/index.html', {'books': books})
+
+@login_required
+def add_to_cart(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    cart = request.session.get('cart', [])
+
+    if book.id not in cart:
+        cart.append(book.id)
+
+    request.session['cart'] = cart 
+    return render(request, 'books/detail.html', {'book': book})
+
+
+def remove_from_cart(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    cart = request.session.get('cart', [])
+
+    if book_id in cart:
+        cart.remove(book_id)
+
+    request.session['cart'] = cart  
+    return render(request, 'cart/index.html', {'book': book})
+
 
 class BookCreate(LoginRequiredMixin,CreateView):
     model = Book
